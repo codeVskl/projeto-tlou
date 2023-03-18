f9a8c78bf58d63a99429bb74b213d3c0db25c828
codeVskl <jpzito72@gmail.com>
2023-03-18 16:49:45 -0300

    first commit

---
+++ src/JS/index.js
@@ -0,0 +1,35 @@
+//OBJETIVO - quando clicar no botão mostrar a imagem de fundo correspondente
+
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem')
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrossel(botao);
+
+        esconderImagemAtiva();
+        
+        mostrarImagemDeFundo(indice);
+    })
+})
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativo');
+}
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativo');
+    imagemAtiva.classList.remove('ativo');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
